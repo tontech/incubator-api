febf1913343dcd4cc06cd8638f19d7f51c050f61
tontech <60031260+tontech@users.noreply.github.com>
2023-04-11 11:40:23 +0800

    Delete pushToCF.sh

--- pushToCF.sh
+++
@@ -1,5 +0,0 @@
-
-ibmcloud api https://api.us-south.cf.cloud.ibm.com
-ibmcloud login -u antoniogualinjr@gmail.com -p nakalimutankoA1!
-ibmcloud target -o antoniogualinjr@gmail.com -s dev
-ibmcloud cf push
